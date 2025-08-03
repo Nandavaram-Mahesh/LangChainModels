40881a832927644d2edf006273aa1457c7d378e6
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2025-08-04 03:29:42 +0530

    OpenApi Key added and made related changes in both openai llm and chat models code

--- ChatModels/1_chatmodel_openai.py
+++ ChatModels/1_chatmodel_openai.py
@@ -1,9 +1,16 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
-model = ChatOpenAI(model='gpt-4', temperature=1.5, max_completion_tokens=10)
+# Get token from environment
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+if not openai_api_key:
+    raise ValueError("openai_api_key not found. Please set the openai_api_key environment variable.")
+
+model = ChatOpenAI(model='gpt-4.0', temperature=1.5, max_completion_tokens=10, openai_api_key=openai_api_key)
 
 result = model.invoke("Write a 5 line poem on cricket")
 
--- LLMs/1_llm_OpenApi.py
+++ LLMs/1_llm_OpenApi.py
@@ -1,14 +1,21 @@
 from langchain_openai import OpenAI
 from dotenv import load_dotenv
+import os
 
 # This is Paid Veresion , which Requires API Key
 load_dotenv()
+
+# Get token from environment
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+if not openai_api_key:
+    raise ValueError("openai_api_key not found. Please set the openai_api_key environment variable.")
 
 llm = OpenAI(
     model="gpt-3.5-turbo-instruct",
     temperature=0,
     # max_retries=2,
-    # api_key="...",
+    api_key=openai_api_key,
     # base_url="...",
     # organization="...",
     # other params...
