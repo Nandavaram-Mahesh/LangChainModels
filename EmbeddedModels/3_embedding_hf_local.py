9ec42477bf972843967bc4e3dc4c8205604b7436
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2025-08-11 04:15:52 +0530

    Added Comments in HuggingFace Embedding Code

--- EmbeddedModels/3_embedding_hf_local.py
+++ EmbeddedModels/3_embedding_hf_local.py
@@ -8,6 +8,11 @@ documents = [
     "Paris is the capital of France"
 ]
 
+# If single Query
+# query = "What is the capital of India?"
+# vector = embedding.embed_query(query)
+
+#If list of Queries/Documents 
 vector = embedding.embed_documents(documents)
 
 print(str(vector))
